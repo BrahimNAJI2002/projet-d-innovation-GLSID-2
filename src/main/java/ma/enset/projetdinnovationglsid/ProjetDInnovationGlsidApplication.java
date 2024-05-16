14937b7065eb8066c2c9cdaa8636be11f40a7e0f
BrahimNAJI2002 <brahim.naji1-etu@etu.univh2c.ma>
2024-05-16 10:29:01 +0100

    Création du projet

---
+++ src/main/java/ma/enset/projetdinnovationglsid/ProjetDInnovationGlsidApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.projetdinnovationglsid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDInnovationGlsidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetDInnovationGlsidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/projetdinnovationglsid/ProjetDInnovationGlsidApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.projetdinnovationglsid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDInnovationGlsidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
